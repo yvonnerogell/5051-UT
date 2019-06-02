d40dd23724e1988b8d435f4dc80d4a44a8cad6b4
Yvonne <yvonne.rogell@gmail.com>
2019-06-02 10:49:55 -0700

    Added ReportViewModelTests.cs to the project.

---
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HW1c.Models;
+
+namespace UnitTests.Models
+{
+    [TestClass]
+    public class ReportViewModelTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
