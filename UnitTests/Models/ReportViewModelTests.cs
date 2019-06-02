e3ce844407925e510c1fc55855183318a45ce8b9
Yvonne <yvonne.rogell@gmail.com>
2019-06-02 11:06:36 -0700

    Added ReportViewModel_NumberOfUsers Get test (should pass).

--- UnitTests/Models/ReportViewModelTests.cs
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -20,5 +20,20 @@ namespace UnitTests.Models
             Assert.IsNotNull(result);
 
         }
+
+        [TestMethod]
+        public void ReportViewModel_NumberOfUsers_Get_Default_Should_Pass()
+        {
+            // Arrange
+            var myReportViewModel = new ReportViewModel();
+
+            // Act
+            int numUsersTest = myReportViewModel.NumberOfUsers;
+
+            // Assert
+            // Should be zero users when it is first started
+            Assert.AreEqual(0, numUsersTest);
+        }
+
     }
 }
