f91f3fd242591b0316d3df572eeb09018cb372eb
Yvonne <yvonne.rogell@gmail.com>
2019-06-02 10:41:45 -0700

    Added LogViewModel Get Test.

--- UnitTests/Models/LogViewModelTests.cs
+++ UnitTests/Models/LogViewModelTests.cs
@@ -19,5 +19,20 @@ namespace UnitTests.Models
             Assert.IsNotNull(result);
 
         }
+
+        [TestMethod]
+        public void LogViewModel_Get_Default_Should_Pass()
+        {
+
+            // Arrange
+            var myTest = new LogViewModel();
+
+            // Act
+            var result = myTest.LogList;
+            
+            // Assert           
+            Assert.IsNotNull(result);
+
+        }
     }
 }
