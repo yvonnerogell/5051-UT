d9fb8f22f737e8d5e3f4c462f8a75c1c5340a4cd
Yvonne <yvonne.rogell@gmail.com>
2019-06-02 11:30:22 -0700

    Added test to ReportViewModel for Get LogViewModel (should pass).

--- UnitTests/Models/ReportViewModelTests.cs
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -35,5 +35,35 @@ namespace UnitTests.Models
             Assert.AreEqual(0, numUsersTest);
         }
 
+        [TestMethod]
+        public void ReportViewModel_NumberOfUsers_Set_Default_Should_Pass()
+        {
+            // Arrange
+            var myReportViewModel = new ReportViewModel();
+
+            // Act
+            // Set number of users to 5
+            myReportViewModel.NumberOfUsers = 5;
+
+            // Assert
+            // Should be 5
+            Assert.AreEqual(5, myReportViewModel.NumberOfUsers);
+        }
+
+        [TestMethod]
+        public void ReportViewModel_LogViewModel_Get_Default_Should_Pass()
+        {
+            // Arrange
+            var myReportViewModel = new ReportViewModel();
+
+            // Act
+            var myLogViewModel = myReportViewModel.LogViewModel;
+
+            // Assert
+            // Should NOT be null
+            Assert.IsNotNull(myLogViewModel);
+
+        }
+
     }
 }
