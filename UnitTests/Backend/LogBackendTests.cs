3000bb30f403194ee1aae4d8691c78bb8136955f
Yvonne <yvonne.rogell@gmail.com>
2019-06-02 14:33:11 -0700

    Added test for LogBackend_Index_Default_Should_Pass().

--- UnitTests/Backend/LogBackendTests.cs
+++ UnitTests/Backend/LogBackendTests.cs
@@ -12,12 +12,27 @@ namespace UnitTests.Backend
         public void LogBackend_Default_Instance_Should_Pass()
         {
             // Arrange
+            // Can't instantiate a singleton pattern
 
             // Act
             var result = LogBackend.Instance;
 
             // Assert
             Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void LogBackend_Index_Default_Should_Pass()
+        {
+            // Arrange
+            // Can't instantiate a singleton pattern
+
+            // Act
+            LogViewModel myData = LogBackend.Instance.Index();
+
+            // Assert
+            // Should have 4 rows
+            Assert.AreEqual(4, myData.LogList.Count);
         }
     }
 }
