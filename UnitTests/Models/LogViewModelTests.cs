243787a235ef950b47fe882e5a14a5b3e1fa6aa2
Yvonne <yvonne.rogell@gmail.com>
2019-06-02 10:47:17 -0700

    Added LogViewModel Set Test.

--- UnitTests/Models/LogViewModelTests.cs
+++ UnitTests/Models/LogViewModelTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using HW1c.Models;
+using System.Collections.Generic;
 
 namespace UnitTests.Models
 {
@@ -32,6 +33,24 @@ namespace UnitTests.Models
             
             // Assert           
             Assert.IsNotNull(result);
+
+        }
+
+        [TestMethod]
+        public void LogViewModel_Set_Default_Should_Pass()
+        {
+
+            // Arrange
+            var myTest = new LogViewModel();
+            var myList = new List<LogModel>();
+            myList.Add(new LogModel { PhoneID = "Phone" });
+
+            // Act
+            myTest.LogList = myList;
+            var result = myTest.LogList;
+
+            // Assert           
+            Assert.AreEqual("Phone", result[0].PhoneID);
 
         }
     }
