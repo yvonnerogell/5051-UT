92c0417ae6fe32e5be6534ca1273c4f48c99179d
Yvonne <yvonne.rogell@gmail.com>
2019-06-02 12:46:08 -0700

    Added LogBackendTests.cs to project.

---
+++ UnitTests/Backend/LogBackendTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HW1c.Backend;
+
+namespace UnitTests.Backend
+{
+    [TestClass]
+    public class LogBackendTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
